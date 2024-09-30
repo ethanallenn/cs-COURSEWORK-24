265a3839a7109d5cd78b3fe56d7fba9aa30c42b2
Ethan Allen <eallen545@c2kni.net>
2024-09-30 08:46:28 +0100

    Update header_config.py

--- header_config.py
+++ header_config.py
@@ -26,3 +26,19 @@ if __name__ == "__main__":
     head.update_header("Inventory")
     
     root.mainloop()
+
+# This class will be able to change the header shown depending on what functionality is being used,
+# e.g., if in the inventory section, it will display "Pharmacy Manager - Inventory".
+class Header(tk.LabelFrame):
+    
+    # 'header' class will constantly update the location in which the user is accessing.
+    def __init__(self, master, **kargs):
+        super().__init__(master, **kargs)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        
+        self.head_label = tk.Label(self, text="Pharmacy Manager")
+        self.head_label.grid(row=0, column=0)
+
+    def update_header(self, section):
+        self.head_label.config(text=f"Pharmacy Manager - {section}")
